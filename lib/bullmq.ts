648c7c3bf05280d2e755d5182980cf80bd600b92
Dhruva <rdhruva12345@gmail.com>
2025-09-02 15:44:22 +0530

    fix: join and register team

--- lib/bullmq.ts
+++ lib/bullmq.ts
@@ -2,6 +2,24 @@ import { Queue, Worker } from "bullmq";
 import { redis } from "./redis";
 import { prisma } from "./prisma";
 import { client } from "@/client";
+import { Scrim } from "@prisma/client";
+import { TextChannel } from "discord.js";
+import { createSlotListEmbed } from "@/commands/registerteam";
+
+export async function sendInitialSlotListEmbed(scrim: Scrim) {
+  const channel = await client.channels.fetch(scrim.registrationChannelId);
+  if (!channel || !(channel instanceof TextChannel)) {
+    throw new Error("Registration channel not found or is not a text channel.");
+  }
+
+  const slotListEmbed = await createSlotListEmbed(scrim);
+  const message = await channel.send({ embeds: [slotListEmbed] });
+
+  await prisma.scrim.update({
+    where: { id: scrim.id },
+    data: { slotListMessageId: message.id },
+  });
+}
 
 export const worker = new Worker(
   "discord-jobs",
@@ -34,9 +52,10 @@ export const worker = new Worker(
           },
         ],
       });
+      await sendInitialSlotListEmbed(scrim);
     }
   },
-  { connection: redis },
+  { connection: redis }
 );
 
 export const queue = new Queue("discord-jobs", {
