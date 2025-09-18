95d7ebaca8f93bcf43cda53f842878d269a41b4b
Shivang Rathore <rshivang12345@gmail.com>
2025-09-18 20:38:31 +0530

    feat: add timezone

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,18 @@ export const TIMEZONES = ["IST", "UTC"] as const;
 export const BRAND_COLOR = 0x8b47fb;
 
 export const SCRIM_REGISTRATION_START = "scrim_registration_start";
+
+export const popularTimeZones = [
+  { label: "UTC", value: "UTC" },
+  { label: "US Eastern (New York)", value: "America/New_York" },
+  { label: "US Central (Chicago)", value: "America/Chicago" },
+  { label: "US Pacific (Los Angeles)", value: "America/Los_Angeles" },
+  { label: "Europe London", value: "Europe/London" },
+  { label: "Europe Berlin", value: "Europe/Berlin" },
+  { label: "Europe Paris", value: "Europe/Paris" },
+  { label: "Europe Moscow", value: "Europe/Moscow" },
+  { label: "India (IST)", value: "Asia/Kolkata" },
+  { label: "China (Beijing)", value: "Asia/Shanghai" },
+  { label: "Japan (Tokyo)", value: "Asia/Tokyo" },
+  { label: "Australia (Sydney)", value: "Australia/Sydney" },
+];
