4fda713b9bf09b13c000cecbef886ae29fae6c58
Dhruva <rdhruva12345@gmail.com>
2025-10-02 01:53:17 +0530

    feat(teamportal): add create team logic to create team component

---
+++ src/events/team-portal/show-create-team-modal.ts
@@ -0,0 +1,62 @@
+import {
+  ActionRowBuilder,
+  Interaction,
+  ModalBuilder,
+  TextInputBuilder,
+} from "discord.js";
+import { Event } from "@/base/classes/event";
+
+function globalTeamConfigModel() {
+  const rows: ActionRowBuilder<TextInputBuilder>[] = [];
+  rows.push(
+    new ActionRowBuilder<TextInputBuilder>().addComponents(
+      new TextInputBuilder()
+        .setCustomId("team_name")
+        .setLabel("Team Name")
+        .setStyle(1)
+        .setMinLength(1)
+        .setMaxLength(100)
+        .setPlaceholder("Enter your global team name")
+        .setRequired(true)
+    )
+  );
+  rows.push(
+    new ActionRowBuilder<TextInputBuilder>().addComponents(
+      new TextInputBuilder()
+        .setCustomId("team_ign")
+        .setLabel("In-Game Name")
+        .setStyle(1)
+        .setMinLength(0)
+        .setMaxLength(100)
+        .setPlaceholder("Enter your in-game name")
+        .setRequired(true)
+    )
+  );
+  rows.push(
+    new ActionRowBuilder<TextInputBuilder>().addComponents(
+      new TextInputBuilder()
+        .setCustomId("team_tag")
+        .setLabel("tag Name")
+        .setStyle(1)
+        .setMinLength(0)
+        .setMaxLength(100)
+        .setPlaceholder("Set your team tag")
+        .setRequired(false)
+    )
+  );
+
+  return new ModalBuilder()
+    .setCustomId(`create_team_submit`)
+    .setTitle("Create Your Team")
+    .addComponents(...rows);
+}
+
+export default class ShowGlobalTeamModel extends Event<"interactionCreate"> {
+  public event = "interactionCreate" as const;
+  async execute(interaction: Interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== "show_create_team_modal") return;
+    const modal = globalTeamConfigModel();
+    await interaction.showModal(modal);
+  }
+}
---
+++ src/events/team-portal/submit-create-team-modal.ts
@@ -0,0 +1,92 @@
+import { prisma } from "@/lib/prisma";
+import { Interaction } from "discord.js";
+import z from "zod";
+import { Event } from "@/base/classes/event";
+import { randomString } from "@/lib/utils";
+import { TeamRole } from "@prisma/client";
+import { isUserBanned } from "@/checks/banned";
+import { ensureUser } from "@/database";
+
+const TeamConfigSchema = z.object({
+  teamName: z.string().min(2).max(32),
+  ign: z.string().max(100),
+  tag: z.string().max(10).optional(),
+});
+
+export default class GlobalTeamModelSubmit extends Event<"interactionCreate"> {
+  public event = "interactionCreate" as const;
+  async execute(interaction: Interaction) {
+    if (!interaction.isModalSubmit()) return;
+    if (interaction.customId !== "create_team_submit") return;
+
+    await interaction.deferReply({ flags: ["Ephemeral"] });
+
+    const rawBody = {
+      teamName: interaction.fields.getTextInputValue("team_name"),
+      ign: interaction.fields.getTextInputValue("team_ign"),
+      tag: interaction.fields.getTextInputValue("team_tag"),
+    };
+
+    const parsed = TeamConfigSchema.safeParse(rawBody);
+    if (!parsed.success) {
+      await interaction.editReply({
+        content: `There was an error with your input: ${parsed.error.issues
+          .map((i) => i.message)
+          .join(", ")}`,
+      });
+      return;
+    }
+    const guildConfig = await prisma.guildConfig.findUnique({
+      where: { id: interaction.guildId! },
+    });
+    const maxTeamsPerCaptain = guildConfig?.teamsPerCaptain || 1;
+    const existing = await prisma.team.findFirst({
+      where: {
+        guildId: interaction.guildId!,
+        name: parsed.data.teamName,
+      },
+    });
+    if (existing) {
+      await interaction.editReply({
+        content: `A team with the name **${parsed.data.teamName}** already exists. Please choose a different name.`,
+      });
+      return;
+    }
+    const captainTeamsCount = await prisma.team.count({
+      where: {
+        guildId: interaction.guildId!,
+        teamMembers: {
+          some: { userId: interaction.user.id, role: "CAPTAIN" },
+        },
+      },
+    });
+
+    if (captainTeamsCount >= maxTeamsPerCaptain) {
+      await interaction.editReply({
+        content: `You have reached the maximum number of teams (${maxTeamsPerCaptain}) you can create as a captain.`,
+      });
+      return;
+    }
+    const teamCode = randomString(8);
+    await ensureUser(interaction.user);
+    const data = parsed.data;
+    await prisma.team.create({
+      data: {
+        name: data.teamName,
+        guildId: interaction.guildId!,
+        code: teamCode,
+        tag: data.tag || null,
+        teamMembers: {
+          create: {
+            userId: interaction.user.id,
+            ingameName: data.ign!,
+            role: TeamRole.CAPTAIN,
+          },
+        },
+      },
+    });
+    await interaction.editReply({
+      content: `Your team **${data.teamName}** has been created! Share this code with others to let them join your team: **${teamCode}**`,
+    });
+  }
+}
